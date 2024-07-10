3177e9835fdd6048cd8003cb691ff381883cb5f5
sofia <utm22030544@utma.edu.mx>
2024-07-10 13:56:11 -0600

    feat: repository added

---
+++ src/varuna_backend/src/index.ts
@@ -0,0 +1,10 @@
+import { Server }  from 'azle';
+import express from 'express';
+import cors from 'cors';
+
+export default Server(() => {
+    const app = express();
+
+    app.use(cors());
+    return app.listen();
+})
